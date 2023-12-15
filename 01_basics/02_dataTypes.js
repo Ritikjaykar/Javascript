4e2308119ab94fff24c032e68e265e2d82ffc87c
Ritik Jaykar <113467380+Ritikjaykar@users.noreply.github.com>
2023-12-15 21:06:18 +0000

    dataTypes uploaded

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+
+// alert(5+4) we are using JS not browser
+
+// console.log(5+
+//     4); // code must be readible not matters it is executed or not but it should be readible
+
+let Name = "Ritik"
+let age = 21
+let isLoggedIn =false
+let state;
+
+// number ==>2 to power 53
+// bigint
+// string => true/false
+// null =>standalone value,   ex= temperature
+// symbol => unique
+// undefined =>
+
+console.log(typeof Name);
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //object
