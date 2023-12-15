95c7f294fabd60ff8eb07cffc7cdbda4a5343435
Ritik Jaykar <113467380+Ritikjaykar@users.noreply.github.com>
2023-12-15 21:05:31 +0000

    variables uploaded

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 225554; //used when we want constant variables
+let accountPass ="222222" //used when we want to change the variables and most used syntax
+var accountName = "Ritik"; //not use beacuse of issue in block scope and functional scope and also old style syntax and is not used nowadays
+accountCity ="Patna"; //dont use as this is wrong way to write , it will execute but a wrong way to write
+
+// accountID = 65956654;
+accountPass = "222334";
+accountName = "Jaykar"
+accountCity ="Jaipur"; 
+
+console.log(accountID);
+console.table([accountID,accountName, accountPass, accountCity]);
