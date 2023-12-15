7748adfa7cbd8cbf5d580fc18aa2cdd8714f640b
Ritik Jaykar <113467380+Ritikjaykar@users.noreply.github.com>
2023-12-15 21:08:22 +0000

    conversions uploaded

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score ="Ritik"
+
+// console.log(typeof (score0))
+// console.log(typeof score)
+
+let valueInNumber =Number(score)
+// console.log(typeof score)
+// console.log(typeof (score))
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let isLoggedIn = "Ritik"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(typeof (booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ritik" => true
+
+let somenumber = 1
+
+let stringNumber = String(somenumber)
+console.log(typeof stringNumber);
+console.log(typeof (stringNumber));
+console.log(stringNumber)
