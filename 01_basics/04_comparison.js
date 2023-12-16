778d6b28174d9ec9d4239e0fcac45e91297bfb9f
Ritik Jaykar <113467380+Ritikjaykar@users.noreply.github.com>
2023-12-16 20:23:19 +0000

    uploaded comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+/*
+easy comparisons
+console.log(2==3);
+console.log(2>3);
+console.log(2<3);
+console.log(2!=3);
+console.log(2>=3);
+console.log(2<=3);
+*/
+
+
+/*
+confusing comparisons and most important Unpredictable
+
+console.log("2">2);
+console.log(2<"2");
+
+
+
+console.log(null >0);  => false
+console.log(null ==0); =>false
+console.log(null >=0); =>true
+
+console.log(undefined > 0); =>false
+console.log(undefined ==0); =>false
+console.log(undefined <0);  =>false
+*/
+
+// console.log("2"===2); => false
