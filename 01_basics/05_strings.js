51d0eda4de707da027eb8805b5c5861c29574599
Ritik Jaykar <113467380+Ritikjaykar@users.noreply.github.com>
2023-12-17 19:50:43 +0000

    change made

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -26,7 +26,7 @@ const newStringOne = "   Ritik    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://Ritik.com/hitesh%20jaykar"
 
 console.log(url.replace('%20', '-'))
 
